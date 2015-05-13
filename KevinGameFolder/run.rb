b2cad725f4383260d0107f5f5f97213ee3f97028
Kevin <kxaphirak@gmail.com>
2015-05-13 03:26:21 -0700

    Create run.rb

---
+++ KevinGameFolder/run.rb
@@ -0,0 +1,14 @@
+=begin
+	Kevin Xaphirak
+	Game Title: Bard's Revenge
+	May/04/15
+=end
+
+$: << File.dirname(__FILE__)
+
+require 'gosu'
+require 'rubygems'
+require 'scripts/Game_Window'
+
+window = Game_Window.new
+window.show
